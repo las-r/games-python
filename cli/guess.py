f1fd4c1c7cd2971ea9a3952001cafb28ce09ee30
Nayif Ehan <128936184+las-r@users.noreply.github.com>
2026-03-06 17:24:08 -0500

    Create guess.py

---
+++ cli/guess.py
@@ -0,0 +1,28 @@
+import random
+
+# random number guessing game
+# made by las-r on github
+
+# settings
+rmin, rmax = 1, 1000
+
+
+# game variables
+rand = random.randint(rmin, rmax)
+guess = rmax + 1
+guesses = 0
+
+# game loop
+while guess != rand:
+    guess = int(input("Enter a guess: "))
+    guesses += 1
+    
+    if guess > rand:
+        print("Lower!")
+    elif guess < rand:
+        print("Higher!")
+    else:
+        break
+
+# results
+print(f"Guessed in {guesses} tries.")
